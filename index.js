9e3c8d9312163a404aabe2d64003c5f5f7ed34bf
Cooper Filby <cfilby@cis.uab.edu>
2015-10-27 12:23:16 -0500

    Rename API to fileAPI, add error handling for app

--- index.js
+++ index.js
@@ -2,34 +2,33 @@
 
 var Promise = require('bluebird');
 var express = require('express');
-var humps = require('humps');
 var multer = require('multer');
 var sqlite3 = require('sqlite3');
 
+
+var app = express();
+var db = Promise.promisifyAll(new sqlite3.Database('files.sqlite3'));
 var upload = multer({
   storage: multer.memoryStorage()
 });
-var app = express();
-var db = Promise.promisifyAll(new sqlite3.Database('image.sqlite3'));
 
 db.serializeAsync()
   .then(function() {
-    return db.runAsync('CREATE TABLE IF NOT EXISTS image \
-		(image_id INTEGER PRIMARY KEY ASC, filename TEXT, type TEXT, image BLOB)');
+    return db.runAsync('CREATE TABLE IF NOT EXISTS file \
+		(file_id INTEGER PRIMARY KEY ASC, filename TEXT, type TEXT, content BLOB)');
   })
   .then(function() {
     app.get('/', function(req, res) {
       return res.json({
-        message: 'Welcome to the imageAPI!'
+        message: 'Welcome to the FileAPI!'
       });
     });
 
-    app.get('/images', function(req, res) {
-      db.allAsync('SELECT image_id, type, filename FROM image', [])
+    app.get('/files', function(req, res) {
+      db.allAsync('SELECT file_id AS fileId, type, filename FROM file', [])
         .then(function(results) {
-          var images = humps.camelizeKeys(results);
           return res.json({
-            images: images
+            files: results
           })
         })
         .catch(function(err) {
@@ -40,19 +39,18 @@ db.serializeAsync()
         });
     });
 
-    app.post('/images', upload.single('image'), function(req, res) {
+    app.post('/files', upload.single('file'), function(req, res) {
       if (!req.file) {
         res.status = 400;
         return res.json({
-          message: 'An image is required.'
+          message: 'A file is required.'
         });
       }
 
-			db.runAsync('INSERT INTO image (filename, type, image) VALUES (?, ?, ?)',
-				[req.file.originalname, req.file.mimetype, req.file.buffer])
+      db.runAsync('INSERT INTO file (filename, type, content) VALUES (?, ?, ?)', [req.file.originalname, req.file.mimetype, req.file.buffer])
         .then(function() {
-					// FIXME: Find a way to bind this to promise callback to get
-					// this.lastID. Or maybe use q-sqlite3
+          // FIXME: Find a way to bind this to promise callback to get
+          // this.lastID. Or maybe use q-sqlite3
           res.json({
             message: 'Success!'
           })
@@ -65,27 +63,35 @@ db.serializeAsync()
         });
     });
 
-    app.get('/images/:imageId', function(req, res) {
-      db.getAsync('SELECT image_id, type, image FROM image WHERE image_id = ?', [req.params.imageId])
+    app.get('/files/:fileId', function(req, res) {
+      db.getAsync('SELECT file_id as fileAd, type, content FROM file \
+			 WHERE file_id = ?', [req.params.fileId])
         .then(function(result) {
           res.set('Content-Type', result.type)
-          return res.send(result.image);
+          return res.send(result.content);
         })
         .catch(function(err) {
           res.status(404);
           return res.json({
-            message: 'Image Not Found'
+            message: 'File Not Found'
           });
         });
     });
+
+		app.use(function(err, req, res, next) {
+			res.status(500);
+			return res.json({
+				message: err.message
+			})
+		});
 
     var server = app.listen(3000, function() {
       var host = server.address().address;
       var port = server.address().port;
 
-      console.log('Image Uploader listening at http://%s:%s', host, port);
+      console.log('File REST API listening at http://%s:%s', host, port);
     });
   })
   .catch(function(err) {
-    console.error(err)
+    console.error(err);
   });
